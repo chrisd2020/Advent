613ddffd57af49a2da07ff82f5a36cdec584804f
Chris <chrisd2020@gmail.com>
2024-11-28 01:57:24 +0000

    Add logic to disable future doors, update styling

--- script.js
+++ script.js
@@ -2,8 +2,15 @@ const doors = document.querySelectorAll('.door');
 const modal = document.getElementById('videoModal');
 const videoIframe = document.getElementById('videoIframe');
 const closeButton = document.querySelector('.close-button');
+const date = new Date;
 
 doors.forEach(door => {
+  // Disable future doors
+  if (date.getMonth() + 1 === 11) {
+    if (date.getDate() + '' >= door.textContent) {
+      door.classList.remove('disabled');
+    }
+  }
   door.addEventListener('click', () => {
     const videoUrl = door.dataset.video;
     videoIframe.src = videoUrl;
