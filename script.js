c86ccdcb3007d676e2a5d1d5c1defa61b205910d
Chris <chrisd2020@gmail.com>
2024-11-28 02:28:21 +0000

    Fix dates, hardcode current date to 10th November

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ const doors = document.querySelectorAll('.door');
 const modal = document.getElementById('videoModal');
 const videoIframe = document.getElementById('videoIframe');
 const closeButton = document.querySelector('.close-button');
-const date = new Date;
+
+// Hardcode date to 10th November
+const date = new Date('2024-11-10');
+// const date = new Date;
 
 doors.forEach(door => {
   // Disable future doors
   if (date.getMonth() + 1 === 11) {
-    if (date.getDate() + '' >= door.textContent) {
+    if (date.getDate() >= Number(door.textContent)) {
       door.classList.remove('disabled');
     }
   }
